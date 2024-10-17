112a1ccf3470bef1e5ee10e980d005a2abef8325
Aravindh Dasari <dasariaravindh21@gmail.com>
2024-10-17 18:19:19 +0530

    Intial HelloController Setup

---
+++ src/main/java/com/dukaan/clone/user_service/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.dukaan.clone.user_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/dukaan/clone/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dukaan.clone.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
